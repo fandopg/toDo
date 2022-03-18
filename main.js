fc338463d9f9cb0a522bb3cc03d9cf7c4d0a2a7d
Fernando Padilla <pgarces.fernando@gmail.com>
2022-03-17 21:33:11 -0600

    Recuperar arreglo del localstorage y correccion de funcion eliminar para que de error

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let fechaNavar
-function fechaNav(){
+
+function fechaNav() {
     fecha = new Date(); //Actualizar fecha.
 
     anio = fecha.getFullYear();
@@ -7,10 +8,10 @@ function fechaNav(){
     diaNum = fecha.getDate();
     diaLetra = fecha.getDay();
 
-    let meses = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'];
-    let diaSemana = ['Lunes','Martes','Miercoles','Jueves','Viernes','Sábado','Domingo'];
+    let meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+    let diaSemana = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
 
-    fechaNavar =  diaSemana[diaLetra] + ' ' + diaNum + ' de ' + meses[mes] + ' del ' + anio ;  
+    fechaNavar = diaSemana[diaLetra] + ' ' + diaNum + ' de ' + meses[mes] + ' del ' + anio;
     console.log(fechaNavar);
     document.getElementById('fechaCompleta').innerText = fechaNavar;
 }
@@ -80,23 +81,23 @@ const cambiaEstilo = (id) => {
 
 //Cambia contador de las tareas terminadas 
 const EstadoTareas = () => {
-        let pendientes = tareas.filter(tarea => tarea.Termino == false);
-        let contador = pendientes.length;
-        let pendientesContador = document.getElementById("pendientesContador");
-        pendientesContador.innerText = contador;
-        let completadas = tareas.filter(tarea => tarea.Termino == true);
-        contador = completadas.length;
-        let completadasContador = document.getElementById("completadasContador");
-        completadasContador.innerText = contador;
-    }
+    let pendientes = tareas.filter(tarea => tarea.Termino == false);
+    let contador = pendientes.length;
+    let pendientesContador = document.getElementById("pendientesContador");
+    pendientesContador.innerText = contador;
+    let completadas = tareas.filter(tarea => tarea.Termino == true);
+    contador = completadas.length;
+    let completadasContador = document.getElementById("completadasContador");
+    completadasContador.innerText = contador;
+}
 //Cambia el estado de la tarea en el objeto
 const cambioId = (id, estado) => {
-        let index = tareas.findIndex(tarea => tarea.Id == id);
-        let arregloTemp;
-        tareas[index].Termino = estado; //Cambio a nivel variable
-        arregloTemp = JSON.stringify(tareas);
-        localStorage.setItem('Tareas', arregloTemp);
-    }
+    let index = tareas.findIndex(tarea => tarea.Id == id);
+    let arregloTemp;
+    tareas[index].Termino = estado; //Cambio a nivel variable
+    arregloTemp = JSON.stringify(tareas);
+    localStorage.setItem('Tareas', arregloTemp);
+}
 //Llena input del modal 
 const llenar = (id) => {
     let cadena = id.split('-');
@@ -114,11 +115,11 @@ const guardarModal = () => {
     let arregloTemp;
     TaskModal = document.getElementById('modal').value;
     idModal = document.getElementById('modal-id').textContent;
-    index = tareas.findIndex(tarea => tarea.Id == idModal);    
-    tareas[index].Task = TaskModal;                                 //Cambio a nivel variable
+    index = tareas.findIndex(tarea => tarea.Id == idModal);
+    tareas[index].Task = TaskModal; //Cambio a nivel variable
     arregloTemp = JSON.stringify(tareas);
-    localStorage.setItem('Tareas', arregloTemp);                    //Cambio a nivel LocalStorage
-    document.getElementById(`T-${idModal}`).innerText = TaskModal;  //Cambio a nivel FrondEnd
+    localStorage.setItem('Tareas', arregloTemp); //Cambio a nivel LocalStorage
+    document.getElementById(`T-${idModal}`).innerText = TaskModal; //Cambio a nivel FrondEnd
 }
 
 //Eliminar tarea
@@ -126,15 +127,19 @@ const eliminaTask = (id) => {
     let cadena = id.split('-');
     let idCard = cadena[1];
     let arreglo = tareas.filter(tarea => tarea.Id != idCard);
-    console.log(arreglo);
-    tareas = arreglo;
-    localStorage.setItem('Tareas',tareas);
+    arreglo = JSON.stringify(arreglo);
+    localStorage.setItem('Tareas', arreglo);
     renderizarTareas();
     EstadoTareas();
 }
 
 //Renderiza Heroe
 const renderizarTareas = () => {
+    const tareasLS = localStorage.getItem("Tareas");
+    if (tareasLS) {
+        tareas = JSON.parse(tareasLS);
+    }
+
     const taskEnDiv = cardsTask.children;
     if (taskEnDiv.length > 0) {
         const tarea = Array.from(taskEnDiv);
@@ -160,23 +165,23 @@ function actual() {
     hora = fecha.getHours(); //hora actual
     minuto = fecha.getMinutes(); //minuto actual
     segundo = fecha.getSeconds(); //segundo actual
-    if (hora<10) { //dos cifras para la hora
-       hora="0"+hora;
-       }
-    if (minuto<10) { //dos cifras para el minuto
-       minuto="0"+minuto;
-       }
-    if (segundo<10) { //dos cifras para el segundo
-       segundo="0"+segundo;
-       }
-    //ver en el recuadro del reloj:
-    mireloj = hora+" : "+minuto+" : "+segundo;	
-            return mireloj; 
+    if (hora < 10) { //dos cifras para la hora
+        hora = "0" + hora;
     }
-function actualizar() { //función del temporizador
-mihora=actual(); //recoger hora actual
-mireloj=document.getElementById("reloj"); //buscar elemento reloj
-mireloj.innerHTML=mihora; //incluir hora en elemento
+    if (minuto < 10) { //dos cifras para el minuto
+        minuto = "0" + minuto;
+    }
+    if (segundo < 10) { //dos cifras para el segundo
+        segundo = "0" + segundo;
+    }
+    //ver en el recuadro del reloj:
+    mireloj = hora + " : " + minuto + " : " + segundo;
+    return mireloj;
 }
-setInterval(actualizar,1000); //iniciar temporizador
 
+function actualizar() { //función del temporizador
+    mihora = actual(); //recoger hora actual
+    mireloj = document.getElementById("reloj"); //buscar elemento reloj
+    mireloj.innerHTML = mihora; //incluir hora en elemento
+}
+setInterval(actualizar, 1000); //iniciar temporizador
